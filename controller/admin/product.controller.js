10e3d0633e78841890539abac61669b7192608b5
tlam15282@gmail.com <tlam15282@gmail.com>
2026-01-01 23:38:00 +0700

    update tách nhỏ search controller, mixin view search

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -1,26 +1,25 @@
 const Product = require("../../models/product.model.js");
 const filterStatusHelper = require("../helpers/filterStatus.js");
+const searchHelper = require("../helpers/search.js");
 // GET /admin/products
 module.exports.index= async(req, res)=>{
     // Bộ lọc trạng thái
     const { currentStatus, filterStatus } = filterStatusHelper(req.query);
 
+    // Tìm kiếm
     let find = {
         deleted: false
     }
     if(req.query.status){
-        // thêm field vào object 
-        find.status = req.query.status;
+        find.status = req.query.status;// thêm field vào object 
     }
-    let keyword = "";
-    if(req.query.keyword){
-        keyword = req.query.keyword;
-
-        const regex = new RegExp(keyword, 'i'); // 'i' không phân biệt hoa thường
-        find.title = regex;
+    const objectSearch = searchHelper(req.query);
+    const keyword = objectSearch.keyword;
+    if(objectSearch.regex){
+        find.title = objectSearch.regex; // thêm field vào object 
     }
-
     const products = await Product.find(find)
+    
     res.render("admin/pages/products/index", {
         pageTitle : "Danh sách sản phẩm",
         products: products,
