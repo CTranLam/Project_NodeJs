96dd7ba5b0cca46a9a36df9f6b428b0e3e9d193d
tlam15282@gmail.com <tlam15282@gmail.com>
2026-01-03 20:23:43 +0700

    Ex23 Delete product

--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -22,3 +22,28 @@ if(buttonsChangeStatus.length > 0){
     });
 }
 // End change status
+
+// Delete product
+const buttonsDelete = document.querySelectorAll('[button-delete]');
+if(buttonsDelete.length > 0){
+    const formDeleteItem = document.querySelector('#form-delete-item'); // Lấy form xóa
+    const path = formDeleteItem.getAttribute('data-path');
+    
+    buttonsDelete.forEach(button => {
+        button.addEventListener('click', ()=>{
+            const isConfirmed = confirm('Bạn có chắc muốn xóa sản phẩm này không?');
+            if(isConfirmed){
+                const id = button.getAttribute('data-id');
+                
+                const action = path + `/${id}?_method=DELETE`;
+                formDeleteItem.action = action;
+
+                // submit form xóa đó lên param
+                formDeleteItem.submit();
+            }
+        });
+
+    });
+
+}
+// End delete product
