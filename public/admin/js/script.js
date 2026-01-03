17491bdaedbcea245f37b5e424195dab958a81ca
tlam15282@gmail.com <tlam15282@gmail.com>
2026-01-03 21:15:36 +0700

    Ex23 Soft Delete multi product

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -98,6 +98,13 @@ if(formChangeMulti){
         const checkboxMulti = document.querySelector('[checkbox-multi]');
         const inputsChecked = checkboxMulti.querySelectorAll("input[name='id']:checked");
 
+        const typeChange = e.target.elements.type.value;
+        if(typeChange == "delete-all"){
+            const isConfirmed = confirm('Bạn có chắc muốn xóa các mục đã chọn không?');
+            if(!isConfirmed){
+                return;
+            }
+        }
         if(inputsChecked.length > 0){
             let ids = [];
             const inputIds = formChangeMulti.querySelector("input[name='ids']");
