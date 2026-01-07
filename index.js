4fe9c9c023d627cf5fe604bbda416a550a42e1e6
tlam15282@gmail.com <tlam15282@gmail.com>
2026-01-07 18:02:35 +0700

    Ex26 Config Deploy Vercel

--- index.js
+++ index.js
@@ -27,9 +27,10 @@ app.use(session({ cookie: { maxAge: 60000 }}));
 app.use(flash());
 // End Flash
 
-app.set('views', './views'); 
+app.set('views', `${__dirname}/views`); 
 app.set('view engine' , 'pug');
-app.use(express.static('public'));
+
+app.use(express.static(`${__dirname}/public`));
 
 // App local variables
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
