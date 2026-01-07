da3485a2891904f7712161e683443fa9b32fcf32
tlam15282@gmail.com <tlam15282@gmail.com>
2026-01-07 17:49:03 +0700

    Ex26 Connect Mongo DB Online

--- config/database.js
+++ config/database.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 module.exports.connect = async() => {
     try{
         await mongoose.connect(process.env.MONGODB_URL);
+        console.log('DB name:', mongoose.connection.name);
+
         console.log("Connected to MongoDB");
     }catch(error){
         console.error("Failed to connect to MongoDB", error);
