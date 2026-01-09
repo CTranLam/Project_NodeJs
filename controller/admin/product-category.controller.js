81140f3b6f50aaf15ae251cf18442d818798097a
tlam15282@gmail.com <tlam15282@gmail.com>
2026-01-09 22:00:54 +0700

    Ex30 Feature update product category

--- controller/admin/product-category.controller.js
+++ controller/admin/product-category.controller.js
@@ -65,3 +65,42 @@ module.exports.createProduct= async(req, res)=>{
 
     res.redirect(`${systemConfig.prefixAdmin}/products-category`);
 };
+
+// GET /admin/products-category/edit/:id
+module.exports.edit= async(req, res)=>{
+    try{
+        const id = req.params.id;
+
+        const data = await ProductCategory.findOne({
+            _id : id,
+            deleted : false
+        })
+
+        // console.log(data)
+
+        let find = {
+            deleted: false
+        };
+
+        const records = await ProductCategory.find(find);
+
+        res.render("admin/pages/products-category/edit", {
+            pageTitle : "Chỉnh sửa danh mục sản phẩm",
+            data : data,
+            records : records
+        });
+    }catch(error){
+        res.redirect(`${systemConfig.prefixAdmin}/products-category`)
+    }
+};
+
+// PATCH /admin/products-category/edit/:id
+module.exports.editPatch= async(req, res)=>{
+    const id = req.params.id;
+
+    req.body.position = parseInt(req.body.position);
+
+    await ProductCategory.updateOne({_id: id}, req.body);
+
+    res.redirect(`/admin/products-category/edit/${id}`)
+};
--- router/admin/product-category.router.js
+++ router/admin/product-category.router.js
@@ -15,7 +15,15 @@ const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware")
 
 router.post("/create", upload.single("thumbnail"),
     uploadCloud.uploadCloudinary,
-    // validate.createPost, // midleware
+    validate.createPost, // midleware
     controller.createProduct); // Xử lý tạo sản phẩm mới
+
+router.get("/edit/:id", controller.edit);
+
+router.patch("/edit/:id",
+    upload.single("thumbnail"),
+    uploadCloud.uploadCloudinary,
+    validate.createPost,
+    controller.editPatch);
 
 module.exports = router;
