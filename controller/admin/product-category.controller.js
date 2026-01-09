e3c3c613470884d9496e7a82f0dd3297cfdd2f5b
tlam15282@gmail.com <tlam15282@gmail.com>
2026-01-09 16:56:50 +0700

    Ex29

--- controller/admin/product-category.controller.js
+++ controller/admin/product-category.controller.js
@@ -8,19 +8,41 @@ module.exports.index= async(req, res)=>{
         deleted: false
     }
 
-    const record = await ProductCategory.find(find);
+    function createTree(arr, parentId = "") {
+        const tree = [];
+        arr.forEach((item) => {
+            if (item.parent_id === parentId) {
+            const newItem = item;
+            const children = createTree(arr, item.id);
+            if (children.length > 0) {
+                newItem.children = children;
+            }
+            tree.push(newItem);
+            }
+        });
+        return tree;
+    }
+
+    const records = await ProductCategory.find(find);
 
     res.render("admin/pages/products-category/index", {
         pageTitle : "Danh mục sản phẩm",
-        records : record
+        records : records
     });
 };
 
 // GET /admin/products-category
 module.exports.create= async(req, res)=>{
+    let find = {
+        deleted: false
+    };
+
+    const records = await ProductCategory.find(find);
+
 
     res.render("admin/pages/products-category/create", {
         pageTitle : "Tạo danh mục sản phẩm",
+        records: records
     });
 };
 
